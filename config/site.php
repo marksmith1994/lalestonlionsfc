6dba811ba9314493b8c0a80d7b861e59cbb9df6c
Mark <mark@Marks-iMac.local>
2019-04-04 17:30:15 +0100

    config files

---
+++ config/site.php
@@ -0,0 +1,30 @@
+<?php
+define('DB_SERVER', '213.171.200.99');
+define('DB_USERNAME', 'masterlion');
+define('DB_PASSWORD', 'Saundersfoot1');
+define('DB_DATABASE', 'lalestonlions_co_uk');
+define('PASSWORD_SALT', 'mniVNOylVVsXL9OafxlytpEnqeLp5lZaTEr8zQ6RvIww8a9jwDoSO7roUfMFCior');
+
+
+//Turn stats off - use LGT Google Analytics
+define('STATISTICS_TRACK_PAGE_VIEWS', false);
+
+//Use advanced permissions as default
+define('PERMISSIONS_MODEL', 'advanced');
+
+//Disable Marketplace support And Intellisearch (So it doesn't call to concrete5.org)
+define('ENABLE_MARKETPLACE_SUPPORT', false);
+define('ENABLE_INTELLIGENT_SEARCH_HELP', false);
+define('ENABLE_INTELLIGENT_SEARCH_MARKETPLACE', false);
+define('ENABLE_NEWSFLOW_OVERLAY', false);
+define('ENABLE_APP_NEWS', false);
+
+//Modify a couple of options on content blocks
+define('ENABLE_CUSTOM_DESIGN', false);
+define('ENABLE_AREA_LAYOUTS', false);
+
+//Modify background imagery or not
+define('DASHBOARD_BACKGROUND_INFO', false);
+define('WHITE_LABEL_DASHBOARD_BACKGROUND_SRC', 'none');
+define('URL_REWRITING', true);
+define('URL_REWRITING_ALL', true);
